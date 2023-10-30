a66614352f6c9d0ee23a3b1b77f1b5383db08c25
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-10-30 18:59:03 +0545

    feat:added get products from category

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,5 +1,6 @@
 import Product from "../modal/product.modal.js";
 
+//Get All Product
 export async function getAllProduct(req, res) {
   try {
     var product = await Product.find({});
@@ -16,6 +17,38 @@ export async function getAllProduct(req, res) {
   }
 }
 
+//Get Produt By Category
+export async function getProductByCategory(req, res) {
+  try {
+    let category = req.query.category;
+    if (!category) {
+      return res.status(500).send({
+        message: "Category is required",
+        success: false,
+      });
+    }
+    var product = await Product.find({ category: category.toUpperCase() });
+    if (product.length == 0) {
+      return res.send({
+        message: "No Product Found",
+        product,
+        success: true,
+      });
+    }
+    res.send({
+      message: "Product Fetched Successfully",
+      product,
+      success: true,
+    });
+  } catch (e) {
+    return res.status(500).send({
+      message: "Server Error",
+      success: false,
+    });
+  }
+}
+
+//Add Product
 export async function addProduct(req, res) {
   var product = new Product({ ...req });
   product = await product.save();
--- view/product.view.js
+++ view/product.view.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { addProduct, getAllProduct } from "../controller/product.controller.js";
+import {
+  addProduct,
+  getAllProduct,
+  getProductByCategory,
+} from "../controller/product.controller.js";
 
 const router = express.Router();
 
 router.get("/all", getAllProduct);
+router.get("/", getProductByCategory);
 router.post("/add", addProduct);
 
 export default router;
