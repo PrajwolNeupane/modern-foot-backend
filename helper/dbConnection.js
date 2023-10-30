f106f81bf3900c2001b0884b8d97d2a79702a2e3
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-10-30 17:19:23 +0545

    fixed env name

--- helper/dbConnection.js
+++ helper/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import "dotenv/config";
 
 export default mongoose
-  .connect(`${process.env.DB_URL}`)
+  .connect(`${process.env.MONGODB_URI}`)
   .then(() => {
     console.log("Connected to Mongo DB");
   })
